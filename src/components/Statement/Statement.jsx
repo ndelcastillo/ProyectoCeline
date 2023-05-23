1ca0c46408383f7a8f48ad8d900f3f66b1f4bca9
Nicolas del Castillo <delcastillonico95@gmail.com>
2023-05-23 17:04:24 -0300

    commitCambios

--- src/components/Statement/Statement.jsx
+++ src/components/Statement/Statement.jsx
@@ -25,13 +25,7 @@ function Statement() {
                         <Col className='statementColumn1' xs={12} lg={12}
                         >
                             <p>
-                                La producción en proceso explora las diferentes disciplinas trabajando conceptos e inquietudes que me intrigan a la hora de desarrollarme. Tal
-                                vez de una manera autorreferencial, es importante para mi, retratar en mi obra mis ideas y los procesos que estas sugieren. El trabajo busca
-                                ser un catalizador de aquellos cuestiones que me obligan a repensar lo cotidiano de una manera diferente. Las dualidades, la muerte, su intimidad, el vigilio, el movimiento. De esta manera, la obra sugiere algunos humores, entre textos, fotografías e instalaciones. Por otro lado, la programación para materializar ideas es importante en el proceso de la obra. Como un gesto tecnológico puede conversar con un texto, lo frío y
-                                lo sensible. Estas ficciones que creo en la obra, tratan de representar la realidad de mundo que veo y creo conocer, de historias de amigxs, sus
-                                intimidades, o voces en mi cabeza que pueden tener figuras familiares. La fragilidad de las partes, la sensiblidad de los seres, la abstracción
-                                de los mismos. Entenderme frágil me hace ser mas dosil, y más maleable para comprender estas situaciones. De esta manera busca la obra
-                                ser un reflejo de mis momentos, mis historias.
+                                La producción en proceso explora las diferentes disciplinas trabajando conceptos e inquietudes que me intrigan a la hora de desarrollarme. Tal vez de una manera auto-referencial, es importante para mí, retratar en mi obra mis ideas y los procesos que estas sugieren. El trabajo busca ser un catalizador de aquellos cuestiones que me obligan a repensar lo cotidiano de una manera diferente. Las dualidades, la muerte, su intimidad, la vigilia, el movimiento. De esta manera, la obra sugiere algunos humores, entre textos, fotografías e instalaciones. Por otro lado, la programación para materializar ideas es importante en el proceso de la obra. Como un gesto tecnológico puede conversar con un texto, lo frío y lo sensible. Estas ficciones que creo en la obra, tratan de representar la realidad de mundo que veo y creo conocer, de historias de amigas/os, sus intimidades, o voces en mi cabeza que pueden tener figuras familiares. La fragilidad de las partes, la sensibilidad de los seres, la abstracción de los mismos. Entenderme frágil me hace ser mas dócil, y más maleable para comprender estas situaciones. De esta manera busca la obra ser un reflejo de mis momentos, mis historias.
                             </p>
                         </Col>
                     </Row>
